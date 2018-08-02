a07dce67feb343ac9e27c9780dd92a205bab39d2
fr0stf0x <tang.xuanthao1996@gmail.com>
2018-08-02 09:45:11 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    AddCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
